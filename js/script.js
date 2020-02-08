a3e6d32651f1ddbe78b76978473e3da4b1a4edcf
harshay <harshaykr@hotmail.com>
2020-02-08 18:30:33 +0000

    update

--- js/script.js
+++ js/script.js
@@ -9,12 +9,11 @@ Project Number : 3
 
 //select form name element
 const name_elm           = document.getElementById("name");
-const email_elm           = document.getElementById("email");
+const email_elm          = document.getElementById("mail");
 //select the job role text box on the form
 const job_role_elm       = document.getElementById("title");
 //create a text box for the job role element which will be visible when other is clicked
-const job_role_elm_other =  document.createElement("input");
-job_role_elm_other.type  = "text";
+const job_role_elm_other =  document.getElementById("other_basic_info");
 //create a text box for the job role element which will be visible when other is clicked
 const shirt_elm          =  document.getElementsByClassName("shirt")[0];
 //
@@ -27,7 +26,6 @@ const main_form          = document.getElementsByTagName("form")[0];
 name_elm.focus();
 
 /***********************************************************************************************/
-main_form.insertBefore(job_role_elm_other,shirt_elm);
 
 //hide text box by default
 job_role_elm_other.style.display = "none";
@@ -267,25 +265,25 @@ reg_main.addEventListener('change', (event) => {
 /************************************************************************************************/
 //payment info 
 
-const pay_type_drop            = document.getElementById("payment");
+const pay_type_drop                    = document.getElementById("payment");
 //payment options
-const pay_type_credit_card     = document.getElementById("credit-card");
-const pay_type_paypal          = document.getElementById("paypal");
-const pay_type_bitcoin         = document.getElementById("bitcoin");
+const pay_type_credit_card             = document.getElementById("credit-card");
+const pay_type_paypal                  = document.getElementById("paypal");
+const pay_type_bitcoin                 = document.getElementById("bitcoin");
 //
-const pay_type_credit_card_option  = document.getElementById("cc-num");
-const pay_type_zip_code_option     = document.getElementById("zip");
-const pay_type_cvv_option          = document.getElementById("cvv");
+const pay_type_credit_card_option      = document.getElementById("cc-num");
+const pay_type_zip_code_option         = document.getElementById("zip");
+const pay_type_cvv_option              = document.getElementById("cvv");
 
 //
-const pay_method               = document.querySelector('option[value="select method"]');
-const cc_method                = document.querySelector('option[value="credit card"]');
+const pay_method                       = document.querySelector('option[value="select method"]');
+const cc_method                        = document.querySelector('option[value="credit card"]');
 
 //on page load
 cc_method.setAttribute("selected","selected");
-pay_method.style.display       = "none";
-pay_type_paypal.style.display  = "none";
-pay_type_bitcoin.style.display = "none";
+pay_method.style.display               = "none";
+pay_type_paypal.style.display          = "none";
+pay_type_bitcoin.style.display         = "none";
 
 //function will display all payment options
 const pay_type_drop_display_all = () => { 
@@ -336,9 +334,64 @@ let reg_activities_error_flg     = 0;
 let credit_card_number_error_flg = 0; 
 let zip_code_error_flg           = 0; 
 let cvv_error_flg                = 0; 
+let total_form_errors_flg        = 0; 
 
-//function will flag validation errors with all form elements if they exist
+////////////////////////////////////////////
+const basic_info_parent = document.getElementsByTagName("fieldset")[0]; 
+const payment_info_legend = document.getElementsByTagName("fieldset")[3]; 
+//create and append elements that will display error messages on failed validation
+const name_error_div                 = document.createElement("div");
+const email_error_div                = document.createElement("div");
+const activities_error_div           = document.createElement("div");
+const credit_card_error_div          = document.createElement("div");
+const zip_error_div                  = document.createElement("div");
+const cvv_error_div                  = document.createElement("div");
+//insert error message into every error div
+name_error_div.innerHTML             = "*Name field cannot be blank.";
+email_error_div.innerHTML            = "*Please check if email has been entered correctly.";
+activities_error_div.innerHTML       = "*Minimum of 1 selection is required.";
+credit_card_error_div.innerHTML      = "*Creit Card Number entered incorrectly.";
+zip_error_div.innerHTML              = "*Zip Code entered incorrectly.";  
+cvv_error_div.innerHTML              = "*CVV entered incorrectly.";
+//elements next to newly created divs which will contain error messages
+const email_title                    = document.querySelector('label[for="mail"]');
+const job_title                      = document.querySelector('label[for="title"]');
+const expiration_date                      = document.querySelector('label[for="exp-month"]');
+//
+name_error_div.style.color = "red";
+email_error_div.style.color = "red";
+activities_error_div.style.color = "red";
+credit_card_error_div.style.color = "red";
+zip_error_div.style.color = "red";
+cvv_error_div.style.color = "red";
+//
+basic_info_parent.insertBefore(name_error_div,email_title);
+basic_info_parent.insertBefore(email_error_div,job_title);
+form_main.insertBefore(activities_error_div,payment_info_legend);
+pay_type_credit_card.insertBefore(credit_card_error_div,expiration_date);
+pay_type_credit_card.insertBefore(zip_error_div,expiration_date);
+pay_type_credit_card.insertBefore(cvv_error_div,expiration_date);
+//
+//hide all errors by default
+const hide_all_errors = () => { 
+
+    name_error_div.style.display = "none"; 
+    email_error_div.style.display = "none";
+    activities_error_div.style.display = "none";   
+    credit_card_error_div.style.display = "none";
+    zip_error_div.style.display = "none";
+    cvv_error_div.style.display = "none";
+
+}; 
+
+//on page load
+hide_all_errors();
+
+////////////////////////////////////////////
+//function will test for validation errors with all form elements and display error messages
 const error_test = () => {
+
+    hide_all_errors();
 
     //reset error flags for every execution
     name_error_flg               = 0; 
@@ -347,50 +400,64 @@ const error_test = () => {
     credit_card_number_error_flg = 0; 
     zip_code_error_flg           = 0; 
     cvv_error_flg                = 0; 
+    // total number of errors on the form
+    total_form_errors_flg        = 0;
 
     if(name_elm.value === "" ){
         
-        name_error_flg = 1;         
+        name_error_flg = 1;    
+        name_error_div.style.display = "";      
     };
     if(email_regex.test(email_elm.value) === false ){
         
         email_error_flg = 1;         
+        email_error_div.style.display = "";  
     };
     if(reg_total_cost_func() === 0 ){
         
-        reg_activities_error_flg = 1;         
+        reg_activities_error_flg = 1;   
+        activities_error_div.style.display = "";      
     };
     if(credit_card_number_regex.test(pay_type_credit_card_option.value) === false ){
         
-        credit_card_number_error_flg = 1;         
+        credit_card_number_error_flg = 1;     
+        credit_card_error_div.style.display = "";
+
     };
     if(zip_code_regex.test(pay_type_zip_code_option.value) === false ){
         
-        zip_code_error_flg = 1;         
+        zip_code_error_flg = 1;       
+        zip_error_div.style.display = "";  
     };
-    if(cvv_regex.test(pay_type_cvv_option.value) === "" ){
+    if(cvv_regex.test(pay_type_cvv_option.value) === false ){
         
-        cvv_error_flg = 1;         
+        cvv_error_flg = 1;     
+        cvv_error_div.style.display = "";    
     };
+
+    total_form_errors_flg = name_error_flg + email_error_flg  + reg_activities_error_flg + credit_card_number_error_flg + zip_code_error_flg + cvv_error_flg;
+
+    return total_form_errors_flg;
 };
 
 /************************************************************************************************/
-//validation error messages on form submission
 
+
+//validation error messages on form submission
 const submit_button_elm = document.querySelector('button[type="submit"]');
 
-submit_button_elm.addEventListener("click", () => {
+submit_button_elm.addEventListener("click", (event) => {
 
+    //check for validation errors
     error_test();
 
-    console.log(name_error_flg)              ; 
-    console.log(email_error_flg)              ; 
-    console.log(reg_activities_error_flg)     ; 
-    console.log(credit_card_number_error_flg) ; 
-        console.log(zip_code_error_flg)       ; 
-            console.log(cvv_error_flg)        ; 
+    //block form submission if validation test fails
+    if(total_form_errors_flg !== 0){
 
-
+        event.preventDefault();
+    };
+    
+       
 
 });
 
